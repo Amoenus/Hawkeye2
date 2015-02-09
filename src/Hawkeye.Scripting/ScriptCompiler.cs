bb5cd4484952cf2f236788d68ebda360d6e824f5
BertTank <awaescher@gmx.de>
2015-02-09 08:37:02 +0100

    extracted the compiler logic to a .NET 4.5.1 assembly via COM to support lambdas

---
+++ src/Hawkeye.Scripting/ScriptCompiler.cs
@@ -0,0 +1,26 @@
+﻿using Hawkeye.Scripting.Proxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hawkeye.Scripting
+{
+	[ComVisible(true)]
+	[ProgId("Hawkeye.Scripting")]
+	[Guid("663DB9DE-E53A-4437-A230-7281C0973EF1")]
+    public class ScriptCompiler : IScriptCompiler
+    {
+		public ScriptCompiler()
+		{
+
+		}
+
+		public string Ping()
+		{
+			return DateTime.Now.ToString();
+		}
+    }
+}

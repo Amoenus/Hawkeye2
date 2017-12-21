c6bd59c0062bbbc778e1c2ab9ccfdc29a3d7dba2
Romans Pokrovskis <romans.pokrovskis@agepartnership.com>
2017-12-21 12:02:10 +0000

    Some more refactoring

--- src/Hawkeye.Core/ComponentModel/descriptors/TypePropertyDescriptor.cs
+++ src/Hawkeye.Core/ComponentModel/descriptors/TypePropertyDescriptor.cs
@@ -5,42 +5,63 @@ namespace Hawkeye.ComponentModel
 {
     internal class TypePropertyDescriptor : PropertyDescriptor
     {
-        private Type ownerType = null;
-        private const string propertyName = "(Type)";
+        private const string PropertyName = "(Type)";
+        private readonly Type _ownerType;
 
+        /// <summary>
+        ///     Initializes a new instance of the
+        ///     <see cref="TypePropertyDescriptor" /> class.
+        /// </summary>
+        /// <param name="ownerObject">The owner object.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     <paramref name="ownerObject" />
+        /// </exception>
+        /// <inheritdoc />
         public TypePropertyDescriptor(object ownerObject)
-            : base(propertyName, null)
+            : base(PropertyName, null)
         {
-            if (ownerObject == null) throw new ArgumentNullException(nameof(ownerObject));
-            ownerType = ownerObject.GetType();
+            if (ownerObject == null)
+            {
+                throw new ArgumentNullException(nameof(ownerObject));
+            }
+
+            _ownerType = ownerObject.GetType();
         }
 
+        /// <inheritdoc />
+        public override Type ComponentType => _ownerType;
+
+        /// <inheritdoc />
+        public override bool IsReadOnly => true;
+
+        /// <inheritdoc />
+        public override Type PropertyType => typeof(Type);
+
+        /// <inheritdoc />
         public override bool CanResetValue(object component)
         {
             return false;
         }
 
-        public override Type ComponentType => ownerType;
-
+        /// <inheritdoc />
         public override object GetValue(object component)
         {
-            return ownerType;
+            return _ownerType;
         }
 
-        public override bool IsReadOnly => true;
-
-        public override Type PropertyType => typeof(Type);
-
+        /// <inheritdoc />
         public override void ResetValue(object component)
         {
             throw new NotSupportedException();
         }
 
+        /// <inheritdoc />
         public override void SetValue(object component, object value)
         {
             throw new NotSupportedException();
         }
 
+        /// <inheritdoc />
         public override bool ShouldSerializeValue(object component)
         {
             return false;

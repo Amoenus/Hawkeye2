20f2e834ffa90ff42e4ae076b4e50abe2b6ab750
Romans Pokrovskis <romans.pokrovskis@agepartnership.com>
2017-12-21 12:17:59 +0000

    More refactoring

--- src/Hawkeye.Core/Logging/log4net/Log4NetHelper.cs
+++ src/Hawkeye.Core/Logging/log4net/Log4NetHelper.cs
@@ -6,16 +6,18 @@ namespace Hawkeye.Logging.log4net
     internal static class Log4NetHelper
     {
         /// <summary>
-        /// Converts a Sopra flavor trace level (<see cref="LogLevel"/>)
-        /// into a log4net like level (<see cref="E:log4net.Core.Level"/>).
+        ///     Converts a Sopra flavor trace <paramref name="level" /> (
+        ///     <see cref="LogLevel" /> ) into a log4net like
+        ///     <paramref name="level" /> ( <see cref="Level" /> ).
         /// </summary>
         /// <param name="level">The trace level to convert.</param>
-        /// <returns>log4net converted trace level.</returns>
+        /// <returns>
+        ///     log4net converted trace level.
+        /// </returns>
         public static Level LogLevelToLog4NetLevel(LogLevel level)
         {
             switch (level)
             {
-                case LogLevel.All: return Level.All;
                 case LogLevel.Verbose: return Level.Verbose;
                 case LogLevel.Debug: return Level.Debug;
                 case LogLevel.Info: return Level.Info;
@@ -23,44 +25,83 @@ namespace Hawkeye.Logging.log4net
                 case LogLevel.Error: return Level.Error;
                 case LogLevel.Fatal: return Level.Fatal;
                 case LogLevel.Off: return Level.Off;
+                case LogLevel.All: return Level.All;
+                default: return Level.All;
             }
-
-            return Level.All;
         }
 
         /// <summary>
-        /// Converts a log4net flavor trace level (<see cref="E:log4net.Core.Level"/>)
-        /// into a Sopra like level (<see cref="LogLevel"/>).
+        ///     Converts a log4net flavor trace <paramref name="level" /> (
+        ///     <see cref="Level" /> ) into a Sopra like <paramref name="level" /> (
+        ///     <see cref="LogLevel" /> ).
         /// </summary>
         /// <param name="level">The log4net level to convert.</param>
-        /// <returns>Sopra converted trace level.</returns>
+        /// <returns>
+        ///     Sopra converted trace level.
+        /// </returns>
         public static LogLevel Log4NetLevelToLogLevel(Level level)
         {
-            if (level == Level.All) return LogLevel.All;
-            if (level == Level.Verbose) return LogLevel.Verbose;
-            if (level == Level.Debug) return LogLevel.Debug;
-            if (level == Level.Info) return LogLevel.Info;
-            if (level == Level.Warn) return LogLevel.Warning;
-            if (level == Level.Error) return LogLevel.Error;
-            if (level == Level.Fatal) return LogLevel.Fatal;
-            if (level == Level.Off) return LogLevel.Off;
+            if (level == Level.All)
+            {
+                return LogLevel.All;
+            }
+
+            if (level == Level.Verbose)
+            {
+                return LogLevel.Verbose;
+            }
+
+            if (level == Level.Debug)
+            {
+                return LogLevel.Debug;
+            }
+
+            if (level == Level.Info)
+            {
+                return LogLevel.Info;
+            }
+
+            if (level == Level.Warn)
+            {
+                return LogLevel.Warning;
+            }
+
+            if (level == Level.Error)
+            {
+                return LogLevel.Error;
+            }
+
+            if (level == Level.Fatal)
+            {
+                return LogLevel.Fatal;
+            }
+
+            if (level == Level.Off)
+            {
+                return LogLevel.Off;
+            }
+
             return LogLevel.All;
         }
 
         /// <summary>
-        /// Loggings a log4net flavor logging event (<see cref="LoggingEvent"/>)
-        /// into a Sopra Log entry (<see cref="LogEntry"/>).
+        ///     Loggings a log4net flavor logging event (
+        ///     <see cref="LoggingEvent" /> ) into a Sopra Log entry (
+        ///     <see cref="LogEntry" /> ).
         /// </summary>
         /// <param name="loggingEvent">The logging event.</param>
-        /// <returns></returns>
+        /// <returns>
+        /// </returns>
         public static LogEntry LoggingEventToLogEntry(LoggingEvent loggingEvent)
         {
-            if (loggingEvent == null) return null;
+            if (loggingEvent == null)
+            {
+                return null;
+            }
 
-            var message = loggingEvent.MessageObject == null ?
-                loggingEvent.RenderedMessage : loggingEvent.MessageObject.ToString();
+            string message = loggingEvent.MessageObject?.ToString() ?? loggingEvent.RenderedMessage;
 
-            return new LogEntry()
+            return new LogEntry
             {
                 Exception = loggingEvent.ExceptionObject,
                 Level = Log4NetLevelToLogLevel(loggingEvent.Level),
@@ -73,11 +114,18 @@ namespace Hawkeye.Logging.log4net
 
         public static LoggingEvent LogEntryToLoggingEvent(LogEntry entry)
         {
-            if (entry == null) return null;
-            if (entry.Tag is LoggingEvent) return (LoggingEvent)entry.Tag;
+            if (entry == null)
+            {
+                return null;
+            }
+
+            if (entry.Tag is LoggingEvent)
+            {
+                return (LoggingEvent) entry.Tag;
+            }
 
             return new LoggingEvent(
-                new LoggingEventData()
+                new LoggingEventData
                 {
                     ExceptionString = entry.Exception.ToFormattedString(),
                     Level = LogLevelToLog4NetLevel(entry.Level),
